caddf66e896ba8d31dbf7958bbdd87d41e97735c
Kieran Everett <kieraneverett03@gmail.com>
2021-03-27 20:47:14 +0000

    Added a simple animation

--- 03-spriteTest/spriteTest.c
+++ 03-spriteTest/spriteTest.c
@@ -4,10 +4,22 @@
 
 void main() {
 
+    UINT8 currentSpriteIndex = 0; // unsigned 8-bit integer
+
     set_sprite_data(0, 2, Player);
     set_sprite_tile(0, 0);
     move_sprite(0, 88, 78);
     
     SHOW_SPRITES;
+
+    while (1) {
+        if (currentSpriteIndex == 0) {
+            currentSpriteIndex = 1;
+        }
+        else {
+            currentSpriteIndex = 0;
+        }
+        set_sprite_tile(0, currentSpriteIndex);
+    }
 
 }
