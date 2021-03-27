b1a2bb8125c9074f2f2d225a22fccd264b6e8942
Kieran Everett <kieraneverett03@gmail.com>
2021-03-27 20:43:16 +0000

    Update spriteTest.c

--- 03-spriteTest/spriteTest.c
+++ 03-spriteTest/spriteTest.c
@@ -1,8 +1,13 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "PlayerSprites.c"
 
 void main() {
 
+    set_sprite_data(0, 2, Player);
+    set_sprite_tile(0, 0);
+    move_sprite(0, 88, 78);
     
+    SHOW_SPRITES;
 
 }
