ca731cc9718da1e142f6b933c749148026312406
Kieran Everett <kieraneverett03@gmail.com>
2021-03-28 16:16:39 +0100

    Added a moving sprite that will be the pipe

--- 04-flappyBirdClone/flappyBird.c
+++ 04-flappyBirdClone/flappyBird.c
@@ -14,17 +14,25 @@ void main() {
 
     UINT8 playerSprite = 0;
     INT8 playerLocation[2]; // stores two INT8, x and y of player
+    INT8 pipeLocation[2];
+
     INT8 gravity = -1;
     INT16 currentSpeedY;
-
-    set_sprite_data(0, 1, Bird); // loads player sprites as sprite '0'
-    set_sprite_tile(0, 0); // sets sprite '0' to sprite index '0'
 
     playerLocation[0] = 58;
     playerLocation[1] = 68;
     currentSpeedY = 0;
 
+    pipeLocation[0] = 170;
+    pipeLocation[1] = 100;
+
+    set_sprite_data(0, 1, Bird); // loads player sprites as sprite '0'
+    set_sprite_tile(0, 0); // sets sprite '0' to sprite index '0'
     move_sprite(playerSprite, playerLocation[0], playerLocation[1]); // moves the sprite to roughly the centre of the screen
+
+    set_sprite_data(1, 1, Pipe); // (Location in memory to write, number of tiles to write, where to get the tiles from)
+    set_sprite_tile(1, 1);
+    move_sprite(1, pipeLocation[0], pipeLocation[1]);
     
     DISPLAY_ON;
     SHOW_SPRITES; // shows the sprites on the gb screen
@@ -43,6 +51,9 @@ void main() {
             playerLocation[1] = playerLocation[1] - currentSpeedY;
             move_sprite(playerSprite, playerLocation[0], playerLocation[1]);
         }
+
+        pipeLocation[0] = pipeLocation[0] - 8;
+        move_sprite(1, pipeLocation[0], pipeLocation[1]);
 
         performantDelay(5);
         //delay(100);
