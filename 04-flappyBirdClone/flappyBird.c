664e831b4fa1022fc477c12a1f253132098660e1
Kieran Everett <kieraneverett03@gmail.com>
2021-03-28 00:06:41 +0000

    Added gravity and changed controls

--- 04-flappyBirdClone/flappyBird.c
+++ 04-flappyBirdClone/flappyBird.c
@@ -2,36 +2,48 @@
 #include <stdio.h>
 #include "BirdSprite.c"
 
-#define MOVESPEED 10
+INT8 playerLocation[2]; // stores two INT8, x and y of player
+INT8 gravity = -1;
+INT16 currentSpeedY;
+
+
+void performantDelay(UINT8 numLoops) { // more efficient delay method
+    UINT8 i;
+    for (i = 0; i < numLoops; i++) {
+        wait_vbl_done(); // waits until the frame has been drawn, ie frames
+    }
+}
 
 void main() {
 
-    // UINT8 currentSpriteIndex = 0; // unsigned 8-bit integer
-
-    set_sprite_data(0, 2, Bird); // loads player sprites as sprite '0'
+    set_sprite_data(0, 1, Bird); // loads player sprites as sprite '0'
     set_sprite_tile(0, 0); // sets sprite '0' to sprite index '0'
-    move_sprite(0, 58, 68); // moves the sprite to roughly the centre of the screen
+
+    playerLocation[0] = 58;
+    playerLocation[1] = 68;
+    currentSpeedY = 0;
+
+    move_sprite(0, playerLocation[0], playerLocation[1]); // moves the sprite to roughly the centre of the screen
     
     DISPLAY_ON;
     SHOW_SPRITES; // shows the sprites on the gb screen
 
     while(1) {
-        switch(joypad()){
-            case J_LEFT:
-                scroll_sprite(0, -MOVESPEED, 0);
-                break;
-            case J_RIGHT:
-                scroll_sprite(0, MOVESPEED, 0);
-                break;
-            case J_UP:
-                scroll_sprite(0, 0, -MOVESPEED);
-                break;
-            case J_DOWN:
-                scroll_sprite(0, 0, MOVESPEED);
-                break;
+        
+        if (joypad() & J_A) {
+            currentSpeedY = 5;
         }
 
+        currentSpeedY = currentSpeedY + gravity;
+
+        if (!(playerLocation[1] - currentSpeedY > 160)) {
+            playerLocation[1] = playerLocation[1] - currentSpeedY;
+            move_sprite(0, playerLocation[0], playerLocation[1]);
+        }
+
+        //performantDelay(10);
         delay(100);
+
     }
 
 }
