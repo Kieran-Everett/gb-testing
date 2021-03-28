3fed746a43320ece70cd5b70e18e14bb5162aaf1
Kieran Everett <kieraneverett03@gmail.com>
2021-03-28 16:27:03 +0100

    Making pipe long and moving it

--- 04-flappyBirdClone/flappyBird.c
+++ 04-flappyBirdClone/flappyBird.c
@@ -14,6 +14,8 @@ void main() {
 
     UINT8 playerSprite = 0;
     INT8 playerLocation[2]; // stores two INT8, x and y of player
+
+    UINT8 numberOfPipeSprites = 8;
     INT8 pipeLocation[2];
 
     INT8 gravity = -1;
@@ -23,16 +25,22 @@ void main() {
     playerLocation[1] = 68;
     currentSpeedY = 0;
 
-    pipeLocation[0] = 170;
-    pipeLocation[1] = 100;
+    pipeLocation[0] = 170 -30;
+    pipeLocation[1] = 80;
 
     set_sprite_data(0, 1, Bird); // loads player sprites as sprite '0'
     set_sprite_tile(0, 0); // sets sprite '0' to sprite index '0'
     move_sprite(playerSprite, playerLocation[0], playerLocation[1]); // moves the sprite to roughly the centre of the screen
 
     set_sprite_data(1, 1, Pipe); // (Location in memory to write, number of tiles to write, where to get the tiles from)
-    set_sprite_tile(1, 1);
-    move_sprite(1, pipeLocation[0], pipeLocation[1]);
+    //set_sprite_tile(1, 1); // set sprite number 1 to memory location 1
+    //move_sprite(1, pipeLocation[0], pipeLocation[1]);
+
+    UINT8 i;
+    for (i = 1; i < numberOfPipeSprites; i++) {
+        set_sprite_tile(i, 1);
+        move_sprite(i, pipeLocation[0], pipeLocation[1] + i * 8);
+    }
     
     DISPLAY_ON;
     SHOW_SPRITES; // shows the sprites on the gb screen
@@ -53,7 +61,10 @@ void main() {
         }
 
         pipeLocation[0] = pipeLocation[0] - 8;
-        move_sprite(1, pipeLocation[0], pipeLocation[1]);
+
+        for (i = 1; i < numberOfPipeSprites; i++) {
+            move_sprite(i, pipeLocation[0], pipeLocation[1] + i * 8);
+        }
 
         performantDelay(5);
         //delay(100);
