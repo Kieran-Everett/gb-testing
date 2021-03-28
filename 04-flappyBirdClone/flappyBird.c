8130b66358d89ab66e9dd905d9bf788d5212aaae
Kieran Everett <kieraneverett03@gmail.com>
2021-03-28 15:50:23 +0100

    Reformatting some code

--- 04-flappyBirdClone/flappyBird.c
+++ 04-flappyBirdClone/flappyBird.c
@@ -1,11 +1,7 @@
 #include <gb/gb.h>
 #include <stdio.h>
 #include "BirdSprite.c"
-
-INT8 playerLocation[2]; // stores two INT8, x and y of player
-INT8 gravity = -1;
-INT16 currentSpeedY;
-
+#include "PipeSprite.c"
 
 void performantDelay(UINT8 numLoops) { // more efficient delay method
     UINT8 i;
@@ -16,6 +12,11 @@ void performantDelay(UINT8 numLoops) { // more efficient delay method
 
 void main() {
 
+    UINT8 playerSprite = 0;
+    INT8 playerLocation[2]; // stores two INT8, x and y of player
+    INT8 gravity = -1;
+    INT16 currentSpeedY;
+
     set_sprite_data(0, 1, Bird); // loads player sprites as sprite '0'
     set_sprite_tile(0, 0); // sets sprite '0' to sprite index '0'
 
@@ -23,26 +24,28 @@ void main() {
     playerLocation[1] = 68;
     currentSpeedY = 0;
 
-    move_sprite(0, playerLocation[0], playerLocation[1]); // moves the sprite to roughly the centre of the screen
+    move_sprite(playerSprite, playerLocation[0], playerLocation[1]); // moves the sprite to roughly the centre of the screen
     
     DISPLAY_ON;
     SHOW_SPRITES; // shows the sprites on the gb screen
 
     while(1) {
-        
+
+        // Player controls
         if (joypad() & J_A) {
             currentSpeedY = 5;
         }
 
         currentSpeedY = currentSpeedY + gravity;
 
+        // Player movement
         if (!(playerLocation[1] - currentSpeedY > 160)) {
             playerLocation[1] = playerLocation[1] - currentSpeedY;
-            move_sprite(0, playerLocation[0], playerLocation[1]);
+            move_sprite(playerSprite, playerLocation[0], playerLocation[1]);
         }
 
-        //performantDelay(10);
-        delay(100);
+        performantDelay(5);
+        //delay(100);
 
     }
 
