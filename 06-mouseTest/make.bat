d304b68cf139f9d91993eeab18e710558cfc64e3
Kieran Everett <kieraneverett03@gmail.com>
2021-03-29 19:59:21 +0100

    Update make.bat

--- 06-mouseTest/make.bat
+++ 06-mouseTest/make.bat
@@ -1,2 +1,2 @@
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o flappyBird.o flappyBird.c
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o flappyBird.gb flappyBird.o
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o mouseTest.o mouseTest.c
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o mouseTest.gb mouseTest.o
